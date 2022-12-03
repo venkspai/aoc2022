92ca17cf0a8f50f6cfb711b30518958ee7168f3a
venkspai <venkspai@gmail.com>
2022-12-02 23:08:06 -0500

    New echo file

---
+++ Echo Print/echo.go
@@ -0,0 +1,17 @@
+//Echo prints its command line args
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+	fmt.Println("this program has ended.")
+}
