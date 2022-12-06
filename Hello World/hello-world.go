01f0fbf74e5b99e47ad34ecd6c2498a766855090
venkspai <venkspai@gmail.com>
2022-12-05 21:57:50 -0500

    formatted code with go fmt

--- Hello World/hello-world.go
+++ Hello World/hello-world.go
@@ -1,61 +1,58 @@
 package main
 
 import (
-	"reflect"
 	"fmt"
-    "strconv"
-    "math"
-    "strings"
+	"math"
+	"reflect"
+	"strconv"
+	"strings"
 )
-      
+
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("hello world")
 
-    //declaring variables
-    var number0, number1, number2, number3  = 1, 1.0, 2.2, 0.0
+	//declaring variables
+	var number0, number1, number2, number3 = 1, 1.0, 2.2, 0.0
 
-    var number4 float64 = 3.23
-    number4 = float64(number0)
+	var number4 float64 = 3.23
+	number4 = float64(number0)
 
-    //short declarations
-    strVar := "1000"
-    var myString string
-    var myBool bool
+	//short declarations
+	strVar := "1000"
+	var myString string
+	var myBool bool
 
-    //initializing variables
-    //number1, number2 = 1, 2
+	//initializing variables
+	//number1, number2 = 1, 2
 
+	number3 = number1 + number2
+	fmt.Println("Total is ", number3)
+	fmt.Println("Number 0 is ", number0)
 
-    number3 = number1 + number2
-    fmt.Println("Total is ", number3)
-    fmt.Println("Number 0 is ", number0)
+	intVar, err := strconv.Atoi(strVar)
+	fmt.Println(intVar, err, reflect.TypeOf(intVar))
 
-    
-    intVar, err := strconv.Atoi(strVar)
-    fmt.Println(intVar, err, reflect.TypeOf(intVar))
+	//Using math and strings packages
+	fmt.Println(math.Floor(2.75))
+	fmt.Println(strings.Title("head first go"))
 
+	//Printing a rune
+	fmt.Println('A')
 
-    //Using math and strings packages 
-    fmt.Println(math.Floor(2.75))
-    fmt.Println(strings.Title("head first go"))
+	//Printing Booleans and Integers
+	fmt.Println(true)
+	fmt.Println(23)
 
-    //Printing a rune
-    fmt.Println('A')
+	//printing comparisons
+	fmt.Println(5 == 8)
+	fmt.Println(4+4 == 2)
+	//Printing reflections
+	fmt.Println(reflect.TypeOf(2.5))
 
-    //Printing Booleans and Integers
-    fmt.Println(true);
-    fmt.Println(23)
+	//Printing starting values
+	fmt.Println(myString, myBool)
 
-    //printing comparisons
-    fmt.Println(5==8)
-    fmt.Println(4+4 == 2)
-    //Printing reflections
-    fmt.Println(reflect.TypeOf(2.5))
+	//Printing conversions
+	fmt.Println("Conversion ", number4)
 
-    //Printing starting values
-    fmt.Println(myString, myBool)
-
-    //Printing conversions
-    fmt.Println("Conversion ", number4)
-
-  }
+}
