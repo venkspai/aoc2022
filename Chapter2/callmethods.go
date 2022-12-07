dddda82830bc27157ca43f498d1ac7377f532611
venkspai <venkspai@gmail.com>
2022-12-06 22:16:29 -0500

    Day 1 Part 1 solved

--- Chapter2/callmethods.go
+++ Chapter2/callmethods.go
@@ -1,10 +1,56 @@
 package main
+ 
+import (
+    "bufio"
+    "fmt"
+    "os"
+	"strconv"
+	
+)
+ 
+func main() {
+ 
+	//Declaring variables
+	var tempTotal, number2, max = 0, 0, 0
+	//var err string
 
+    readFile, err := os.Open("data.txt")
+  
+    if err != nil {
+        fmt.Println(err)
+    }
+    fileScanner := bufio.NewScanner(readFile)
+ 
+    fileScanner.Split(bufio.ScanLines)
+  
+    for fileScanner.Scan() {
+   		intVar, _ := strconv.Atoi(fileScanner.Text())
+		if intVar != 0 {
+			number2 = intVar
+			tempTotal = tempTotal + number2 
+		} else	{
+			if tempTotal > max {
+				max = tempTotal
+			}
+			tempTotal = 0
+			number2 = 0
+		}
+		
+		fmt.Println("Max calories: ", max)
+
+    }
+  
+    readFile.Close()
+
+
+/*
 import (
 	"fmt"
 	"time"
 	"reflect"
 	"strings"
+	"bufio"
+	"os"
 )
 
 func main() {
@@ -19,6 +65,14 @@ func main() {
 	broken := "G# R#cks!"
 	fixed := replacer.Replace(broken)
 	fmt.Println(fixed)
-	
+
+	//Reading values from the standard input
+
+	fmt.Print("Enter your grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	os.Re
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Your grade is ", input)
+*/	
 
 }
