3ec6263a584ce1eb2441a7b0e9770eccdbe0624c
venkspai <venkspai@gmail.com>
2022-12-09 23:16:02 -0500

    Solved Part 1 Day 3

---
+++ Day3/rucksacks.go
@@ -0,0 +1,79 @@
+package main
+ 
+import (
+    "bufio"
+    "fmt"
+    "os"
+	"strconv"
+	
+)
+ 
+func main() {
+ 
+	//Declaring variables
+	var tempTotal, number2, max = 0, 0, 0
+	//var err string
+
+    readFile, err := os.Open("data.txt")
+  
+    if err != nil {
+        fmt.Println(err)
+    }
+    fileScanner := bufio.NewScanner(readFile)
+ 
+    fileScanner.Split(bufio.ScanLines)
+  
+    for fileScanner.Scan() {
+   		intVar, _ := strconv.Atoi(fileScanner.Text())
+		if intVar != 0 {
+			number2 = intVar
+			tempTotal = tempTotal + number2 
+		} else	{
+			if tempTotal > max {
+				fmt.Println(">>>>>>>>>>>>>>>>>>>>>>")
+				max = tempTotal
+			}
+			tempTotal = 0
+			number2 = 0
+		}
+		
+		fmt.Println("Max calories: ", max)
+
+    }
+  
+    readFile.Close()
+
+
+/*
+import (
+	"fmt"
+	"time"
+	"reflect"
+	"strings"
+	"bufio"
+	"os"
+)
+
+func main() {
+
+	var now time.Time = time.Now()
+	var year int = now.Year()
+	fmt.Println(year)
+	fmt.Println(reflect.TypeOf(now))
+
+	//Replacer
+	replacer := strings.NewReplacer("#", "o")
+	broken := "G# R#cks!"
+	fixed := replacer.Replace(broken)
+	fmt.Println(fixed)
+
+	//Reading values from the standard input
+
+	fmt.Print("Enter your grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	os.Re
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Your grade is ", input)
+*/	
+
+}
