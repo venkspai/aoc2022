13149162d8326fc85919adabe28d425b4794cf92
venkspai <venkspai@gmail.com>
2022-12-10 20:50:21 -0500

    Solved Part 2 of Day 4

--- Day4/elfgroups.go
+++ Day4/elfgroups.go
@@ -1,6 +1,9 @@
 package main
  
 import (
+	"strconv"
+	"strings"
+	"log"
     "bufio"
     "fmt"
     "os"
@@ -8,72 +11,157 @@ import (
 	
 )
 
-func calcPriority(ruckSack []rune) int {
-	fmt.Println(string(ruckSack))
-	ruckSackLen := len(ruckSack)
-	fmt.Println(ruckSackLen)
 
-	var priority rune
-	var priorityInt int
+func calcOverlap(firstElf []string, secondElf []string) int {
 
-	for i :=0; i < ruckSackLen/2; i++ {
-		for t:=(ruckSackLen/2); t < (ruckSackLen); t++ {
-			if (ruckSack[i] == ruckSack[t]) {
-				priority = ruckSack[i]
-				break
+	//many assumptions about the format
+	//no error handling 
+
+	//7-7 8-42
+	//8-42 7-7
+
+	var overlap int = 0
+	var elfFirstOne, elfFirstTwo, elfSecondOne, elfSecondTwo int = 0, 0, 0, 0
+	var err error
+
+	elfFirstOne, err = strconv.Atoi(firstElf[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	elfFirstTwo, err = strconv.Atoi(firstElf[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	elfSecondOne, err = strconv.Atoi(secondElf[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	elfSecondTwo, err = strconv.Atoi(secondElf[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Numbers:", elfFirstOne, elfFirstTwo, elfSecondOne, elfSecondTwo)
+
+//	5 - 7 .. 7 - 11
+// 7 - 7 .. 7 - 87
+// 7 - 11 .. 5 .. 7
+
+	if elfFirstOne <= elfSecondOne {
+		if elfFirstTwo >= elfSecondOne {
+			overlap = 1
+			fmt.Println("One")
+		}
+	} 
+
+	if elfSecondOne <= elfFirstOne {
+			if elfSecondTwo >= elfFirstOne {
+				overlap = 1
+				fmt.Println("Two")
 			}
 		}
+	
+	fmt.Println(firstElf, secondElf, overlap)
+	return overlap
+
+}
+
+func calcFullyContained(firstElf []string, secondElf []string) int {
+
+	//many assumptions about the format
+	//no error handling 
+
+	//7-7 8-42
+	//8-42 7-7
+
+	var overlap int = 0
+	var elfFirstOne, elfFirstTwo, elfSecondOne, elfSecondTwo int = 0, 0, 0, 0
+	var err error
+
+	elfFirstOne, err = strconv.Atoi(firstElf[0])
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	if (priority > 96) {
-		priorityInt = int(priority - 96)
-	} else {
-		priorityInt = int(priority - 38)
+	elfFirstTwo, err = strconv.Atoi(firstElf[1])
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	elfSecondOne, err = strconv.Atoi(secondElf[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	elfSecondTwo, err = strconv.Atoi(secondElf[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+//	fmt.Println("Numbers:", elfFirstOne, elfFirstTwo, elfSecondOne, elfSecondTwo)
+
+	if elfFirstOne <= elfSecondOne {
+		if elfFirstTwo >= elfSecondTwo {
+			overlap = 1
+//			fmt.Println("One")
+		}
+	} 
+
+	if elfSecondOne <= elfFirstOne {
+			if elfSecondTwo >= elfFirstTwo {
+				overlap = 1
+//				fmt.Println("Two")
+			}
+		}
 	
-	fmt.Println("Priority rune is", priority)
-	fmt.Println("Priority for", string(priority), "is", priorityInt)
-	fmt.Println("")
-	return priorityInt
-	
+//	fmt.Println(firstElf, secondElf, overlap)
+	return overlap
+
 }
+
+
+
+
+func calcElfGroups (elfGroup string) ([]string, []string) {
+	//fmt.Println(elfGroup)
+	res1 := strings.Split(elfGroup, ",")
+	//fmt.Println("First split ", reflect.TypeOf(res1[0]))
+	//fmt.Println("Second split", res1[1])
+
+	firstElf := strings.Split(res1[0], "-")
+	secondElf := strings.Split(res1[1], "-")
+
+	return firstElf, secondElf	
+}
+
  
 func main() {
- 
-	//var err string
+
+	//declare variable
+	var elfGroup int = 0
+	var elfGroup2 int = 0
+
+
 	readFile, err := os.Open("data.txt")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error opening the file: ", err)
+		log.Fatal(err)
 	}
 
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
-  
-	/*
-	Read the file
-	Scan the string
-	read the halves into separate arrays
-	compare arrays to find the common letter
-	capture letter
-	Break
-	Find the priority value associated with that letter
-	total up priority values
-	*/
 
-	var myStringSlice []rune
-	var totalPriority int = 0
+	for fileScanner.Scan() {
+		elfGroup += calcFullyContained(calcElfGroups(string(fileScanner.Text())))
+		elfGroup2 += calcOverlap(calcElfGroups(string(fileScanner.Text())))
+	}
 
-    for fileScanner.Scan() {
-		myStringSlice = ([]rune(fileScanner.Text()))
-		totalPriority = totalPriority + calcPriority(myStringSlice)
-		//fmt.Println(string(myStringSlice))
-    }
+	fmt.Println("Total number of elf groups: ", elfGroup)
+	fmt.Println("Total number of elf overlaps: ", elfGroup2)
 
-	fmt.Println("Total Priority: ", totalPriority)
-
-	
-  
-    readFile.Close()
-
+	readFile.Close()
 
 }
